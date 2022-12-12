9dc9be46c0b16c4420bcf5dbc64a61c80368d363
Shen-Ou Yé <shenou.ye@gmail.com>
2022-12-12 11:57:08 +0100

    rust: finished day 06

---
+++ 2022/rust/day06/src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashSet;
+
+fn find_marker(message: &str, count: usize) -> usize {
+    let mut marker = "".to_string();
+    for candidate in message.chars().collect::<Vec<_>>().windows(count) {
+        let mut chars = HashSet::new();
+        for c in candidate {
+            chars.insert(c);
+        }
+        if chars.len() == count {
+            marker = candidate.iter().collect::<String>();
+            break;
+        }
+    }
+    message.find(marker.as_str()).unwrap() + count
+}
+
+fn main() {
+    // Read the data.
+    let message = include_str!("../../../data/day06.txt")
+        .lines()
+        .next()
+        .unwrap();
+
+    // Part 1.
+    println!("{}", find_marker(message, 4));
+
+    // Part 2.
+    println!("{}", find_marker(message, 14));
+}
