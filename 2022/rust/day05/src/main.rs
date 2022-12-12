0de908fd81e537d99617fcbb53a82e2aa674ae31
Shen-Ou Yé <shenou.ye@gmail.com>
2022-12-12 10:03:58 +0100

    rust: finished day 05

--- 2022/rust/day05/src/main.rs
+++ 2022/rust/day05/src/main.rs
@@ -1,24 +1,24 @@
-use core::str;
-use std::str::FromStr;
-
-struct MoveAction {
-    count: i32,
-    src: i32,
-    dst: i32,
-}
+use std::{fs, str::FromStr};
 
 #[derive(Debug)]
-struct MoveActionError;
+struct ActionError;
 
-impl FromStr for MoveAction {
-    type Err = MoveActionError;
+#[derive(Clone)]
+struct Action {
+    count: usize,
+    src: usize,
+    dst: usize,
+}
+
+impl FromStr for Action {
+    type Err = ActionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let words: Vec<&str> = s.split(' ').collect();
         if words[0] != "move" || words[2] != "from" || words[4] != "to" {
             Err(Self::Err {})
         } else {
-            Ok(MoveAction {
+            Ok(Action {
                 count: words[1].parse().unwrap(),
                 src: words[3].parse().unwrap(),
                 dst: words[5].parse().unwrap(),
@@ -27,46 +27,91 @@ impl FromStr for MoveAction {
     }
 }
 
-fn main() {
-    // Read the data.
-    let lines = include_str!("../../../data/day05.txt").lines();
+#[derive(Clone)]
+struct CrateMover {
+    stacks: Vec<String>,
+    moves: Vec<Action>,
+}
 
-    // Build the starting stacks.
-    let index = lines
-        .clone()
-        .enumerate()
-        .find(|line| line.1.to_string().starts_with(" 1 "))
-        .unwrap()
-        .0;
+impl CrateMover {
+    pub fn from_file(input: &str) -> Self {
+        // Read the data.
+        let file_content = fs::read_to_string(input).unwrap();
+        let lines = file_content.lines();
 
-    let count = (lines.clone().nth(index).unwrap().len() + 1) / 4;
+        // Build the starting stacks.
+        let index = lines
+            .clone()
+            .enumerate()
+            .find(|line| line.1.to_string().starts_with(" 1 "))
+            .unwrap()
+            .0;
 
-    let mut stacks = lines
-        .clone()
-        .take(index)
-        .collect::<Vec<&str>>()
-        .iter()
-        .rev()
-        .fold(vec!["".to_string(); count], |mut stacks, line| {
-            for (i, c) in line.chars().enumerate() {
-                if c.is_ascii_uppercase() {
-                    stacks[(i - 1) / 4].push_str(&format!("{}", c));
+        let count = (lines.clone().nth(index).unwrap().len() + 1) / 4;
+
+        let stacks = lines
+            .clone()
+            .take(index)
+            .collect::<Vec<&str>>()
+            .iter()
+            .rev()
+            .fold(vec!["".to_string(); count], |mut stacks, line| {
+                for (i, c) in line.chars().enumerate() {
+                    if c.is_ascii_uppercase() {
+                        stacks[(i - 1) / 4].push_str(&format!("{}", c));
+                    }
                 }
+                stacks
+            });
+
+        let moves = lines
+            .clone()
+            .skip(count + 1)
+            .map(|line| Action::from_str(line).unwrap())
+            .collect::<Vec<_>>();
+
+        Self { stacks, moves }
+    }
+
+    fn move_crates(mut self, reverse_move: bool) -> String {
+        self.moves.iter().for_each(|action| {
+            let mut s = self.stacks[action.src - 1]
+                .chars()
+                .rev()
+                .take(action.count)
+                .collect::<String>();
+            if !reverse_move {
+                s = s.chars().rev().collect::<String>();
             }
-            stacks
+
+            self.stacks[action.dst - 1].push_str(s.as_str());
+
+            let new_len = self.stacks[action.src - 1].len() - action.count;
+            self.stacks[action.src - 1].truncate(new_len);
         });
 
-    lines.skip(count + 1).for_each(|line| {
-        let move_action = MoveAction::from_str(line).unwrap();
-        // TODO: do the move.
-    });
-
-    println!(
-        "{}",
-        stacks.iter().fold("".to_owned(), |acc, stack| {
+        self.stacks.iter().fold("".to_owned(), |acc, stack| {
             let mut str = acc;
             str.push_str(&format!("{}", stack.chars().last().unwrap()));
             str
         })
-    );
+    }
+
+    pub fn move_9000(self) -> String {
+        self.move_crates(true)
+    }
+
+    pub fn move_9001(self) -> String {
+        self.move_crates(false)
+    }
+}
+
+fn main() {
+    let mover = CrateMover::from_file("../data/day05.txt");
+
+    // Part 1.
+    println!("{}", mover.clone().move_9000());
+
+    // Part 2.
+    println!("{}", mover.move_9001());
 }
